c2adb6a5c6bf91ec35b137f49be210d9fc33ebe8
MatthieuQcc <matthieu.quaccia@gmail.com>
2022-11-09 10:41:57 +0100

    test devMatthieu

--- model.py
+++ model.py
@@ -17,18 +17,45 @@ def regression(pred = False):
     # On récupère le dataFrame
     df = pd.read_csv("./data/value.csv", sep=';')
     
-  
-
     # On enlève les 2 dernières colonnes: les coordonnées 
     df = df.drop(columns=["Coord1", "Coord2"], axis=1)
 
-    df.info()
 
-    print(df.head())
+    X1 = df[["CPU1", "RAM1"]]
+    Y1 = df["Latence1"]
+
+    # On crée le jeu de tests et d'entraînement
+    X_train, X_test, y_train, y_test = train_test_split(X1, Y1, test_size=0.2, random_state=42)
+
+
+    # On normalise les données
+    scaler = StandardScaler().fit(X_train)
+    X_train_transformed = scaler.transform(X_train)
+    X_test_transformed = scaler.transform(X_test)
     
+    minScore = 4000 
+    max_depth = 0
+    bestModel = RandomForestRegressor()
 
-  
+    for i in [1, 2, 3, 4]:
+        for j in [1, 2, 3]:
+            clf = RandomForestRegressor(max_depth=i, min_samples_leaf=j, random_state=0).fit(X_train_transformed, y_train)
+            currentScore = mean_squared_error(y_test, clf.predict(X_test_transformed), squared=False)
+            print(i, j, currentScore)
+            if currentScore < minScore:
+                minScore = currentScore
+                max_depth = i          
+                bestModel = clf 
 
+    print("\nRésultat trouvé : max depth = ", bestModel.max_depth)
+    print("\nRésultat trouvé : min samples leaf = ", bestModel.min_samples_leaf)
+    print("\nScore = ", minScore)
 
+    print("------predictions-------")
+    pred = bestModel.predict(X_test_transformed)
+
+    for i in range(len(pred)):
+        print("pred : ", pred[i], "  true value : ", y_test.to_numpy()[i])
+   
 
 regression()
