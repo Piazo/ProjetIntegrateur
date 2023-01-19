7e07ba11d311985fd4ce73c8fb293c45e01b5d7c
chamaben <chamabenlafkih.cb@gmail.com>
2023-01-19 14:49:56 +0100

    Update modelMicroserviceControler.py

--- Application/Model1Microservice/src/app/modelMicroserviceControler.py
+++ Application/Model1Microservice/src/app/modelMicroserviceControler.py
@@ -1,39 +1,43 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+
 from typing_extensions import Self
 from flask import Flask, jsonify, request
 import random
-import modelMicroserviceModel
-import mysql.connector 
+#import modelMicroserviceModel
+#import mysql.connector 
 
 
 app = Flask(__name__)
 
 class modelMicroserviceControler:
 
-    def predictedLatency(ram, cpu, numNode):
-        latency= abs((ram + cpu - numNode) * random.random())
+    def predictedLatency():
+        latency= random.random()
         return latency
 
-    @app.route("/predict/<int:node>", methods=['POST'])
-    def predict():
-        node = request.get_json(force=True)
-        latency= modelMicroserviceControler.predictedLatency(node["ram"], node["cpu"], node["numNode"])
+    @app.route("/predict", methods=['GET'])
+    def predict(): 
+        args = request.args
+        node = args.get('node')
+        latency= str(modelMicroserviceControler.predictedLatency())
+
         #Connexion à la BDD
-        conn = mysql.connector.connect(host="localhost", user="root", password="Projet2023+", database="latency")
-        cursor=conn.cursor()
+        #conn = mysql.connector.connect(host="localhost", user="root", password="Projet2023+", database="latency")
+        #cursor=conn.cursor()
         # ajouter la dernière latence à la file + supprimer la première
-        cursor.execute("""INSERT INTO latency1(id, valueLatency) VALUES(?, ?)""", (1, 0.0123456789))
-        conn.commit()
-        cursor.execute("""INSERT INTO latency1(id, valueLatency) VALUES(?, ?)""", (2, 0.0987654321))
-        conn.commit()
-        cursor.execute("""DELETE FROM latency1 where id=1""")
-        conn.commit()
-        
-        return jsonify({
-            "latency1": latency
-            })
+        #cursor.execute("""INSERT INTO latency1(id, valueLatency) VALUES(?, ?)""", (1, 0.0123456789))
+        #conn.commit()
+        #cursor.execute("""INSERT INTO latency1(id, valueLatency) VALUES(?, ?)""", (2, 0.0987654321))
+        #conn.commit()
+        #cursor.execute("""DELETE FROM latency1 where id=1""")
+        #conn.commit()
+
+
+        return latency
+
+
 
 
 
