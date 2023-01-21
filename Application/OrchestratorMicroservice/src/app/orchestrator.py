28015bc6d7de6f1ed4d5a9adea44e997e19787f1
hanneoyy <hanneoy.gmail.com>
2023-01-21 20:51:20 +0100

    docker-compose marche sauf bdd

--- Application/OrchestratorMicroservice/src/app/orchestrator.py
+++ Application/OrchestratorMicroservice/src/app/orchestrator.py
@@ -1,5 +1,5 @@
 # from urllib.request import urlopen
-from flask import Flask, jsonify, request
+from flask import Flask, request
 import json
 import requests
 
@@ -8,20 +8,31 @@ app = Flask(__name__)
 class orchestator:
     @app.route("/app/bestnode/", methods=['GET', 'POST'])
     def getBestNode():
-        if request.method == "POST":
-            prev = request.get_json(force=True)
+        # Juste pour tester, en vrai on se connecte au bdd
+        latencies1 = {"prev": [1.22, 0.34, 0.91, 2.59, 1, 1.14]}
+        latencies2 = {"prev": [1.4, 1.4, 1.4, 1.4, 1.4, 1.4]}
+        latencies3 = {"prev": [0.91, 0.91, 0.91, 0.91, 0.91, 0.91]}
+        print(latencies1)
+        print(type(latencies1))
+
         latencyList= []
         bestNode={}
-        #send requests to prediction microservice 
-        for i in range(3):
-            latency= requests.get('http://localhost:50000/predict?node=1')
-            latencyList.append(latency.text)
-        print(latencyList)
-        latency= requests.post('http://model1:5000/predict', json=prev)
-        print(latency.text)
 
+        # Send requests to prediction microservices 
+        predicted1 = requests.post('http://model1:5000/predict', json=latencies1)
+        latencyList.append(predicted1.json())
+        predicted2 = requests.post('http://model2:5000/predict', json=latencies2)
+        latencyList.append(predicted2.json())
+        predicted3 = requests.post('http://model3:5000/predict', json=latencies3)
+        latencyList.append(predicted3.json())
+        
+        print(latencyList)
+        print("pred1:", predicted1)
+        print(".text :", predicted1)
+        
+        # return latencyList # "jusqu'ici ca va <33 "
         #send list of latencies to result processing microservice and return the best nodes
-        bestNode=requests.get('http://localhost:50001/getResult', json=latencyList)
+        bestNode=requests.post('http://processing:5000/getResult', json=latencyList)
         return bestNode.text
 
     @app.route("/app/changezone/", methods=['GET'])
--- Application/ResultProcessingMicroservice/src/app/resultProcessing.py
+++ Application/ResultProcessingMicroservice/src/app/resultProcessing.py
@@ -3,13 +3,13 @@ from flask import Flask, request
 app = Flask(__name__)
 
 class resultProcessingControler(): 
-    @app.route("/getResult")
+    @app.route("/getResult", methods=['POST'])
     #Fonction qui choisit le meilleur noeur parmis ces noeuds
     def best_node():
         best = 10000000
         listNode= request.get_json(force=True)
         for i in range(len(listNode)):
-            value= float(listNode[i])
+            value = float(listNode[i]["latency"])
             if value < best : 
                 best = value
                 bestNode = i+1
