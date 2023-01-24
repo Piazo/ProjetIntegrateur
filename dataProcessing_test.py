30519ca933256c30826ea0a8a5386ff5ca3524f6
Tsjerfell <67692265+Tsjerfell@users.noreply.github.com>
2023-01-24 15:45:34 +0100

    Create dataProcessing_test.py

---
+++ dataProcessing_test.py
@@ -0,0 +1,9 @@
+
+
+# This is a veeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeryyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy loooooooooooooooooooooooooooooong coommmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmment
+def process(x,y):
+ return x
+
+def analyse(x):
+    analysing_wether_or_not_the_input_is_in_the_right_format = (x == True)
+    
