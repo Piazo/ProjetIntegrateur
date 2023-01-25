2535e782dd2a139e7aee6d85986e66308e974692
chamaben <chamabenlafkih.cb@gmail.com>
2023-01-25 14:36:09 +0100

    Delete dataProcessing_test.py

--- dataProcessing_test.py
+++
@@ -1,9 +0,0 @@
-
-
-# This is a veeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeryyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy loooooooooooooooooooooooooooooong coommmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmment
-def process(x,y):
- return x
-
-def analyse(x):
-    analysing_wether_or_not_the_input_is_in_the_right_format = (x == True)
-    
